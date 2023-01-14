47266f74a7f756bed9c5d6b5f7150fff5645d57d
nbeilis <nicole.beilis@gmail.com>
2023-01-14 16:51:21 -0500

    Initial Commit

---
+++ MatchingGame/MatchingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingGameApp.swift
+//  MatchingGame
+//
+//  Created by Nicole Beilis on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct MatchingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
